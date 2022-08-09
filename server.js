e2e96f8ec54836deacb73c9b403b3bcd650850bb
Pietari Eronen <pietari.eronen@nitro.fi>
2022-08-09 14:10:26 +0300

    ReadOnly 3

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults((options) => {
-  options.readOnly = true;
-});
+const middlewares = jsonServer.defaults([{ readOnly: true }]);
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
